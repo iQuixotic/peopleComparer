eeab0f7c38cf8e4c1895fa0ab4f2eda2fa6bc866
Trey <treyw1850@gmail.com>
2019-04-23 09:07:52 -0400

    make sql tables

--- sql/basics.sql
+++ sql/basics.sql
@@ -1,21 +1,14 @@
--- Get everything from the Customers table
-SELECT * FROM Customers;
-
--- Order by a column name in descending order starting with 
---     - Z if alphabetical column
---     - Highest number if numerical column
-SELECT * 
-FROM Country
-ORDER BY Price DESC; -- DESC is Descending
-
--- A list of records of customers and the number of orders any given
--- customer has placed
-SELECT customerName,
-COUNT(*) AS 'number of orders'
-FROM customers
-INNER JOIN orders
-ON orders.customerID = customers.customerID
-GROUP BY customers.customerID;
+CREATE TABLE `comparablepeople_db` . `names`( 
+`id` INT(10) NOT NULL AUTO_INCREMENT ,
+`firstname` VARCHAR(255) NOT NULL , 
+`lastname` VARCHAR(255) NOT NULL , 
+PRIMARY KEY (`id`)) ENGINE = InnoDB;
 
 -- -------------------------------------------------------------------------------------- --
 
+CREATE TABLE `comparablepeople_db` . `stats`( 
+`personId` INT(10) NOT NULL ,
+`height` INT(10) NOT NULL , 
+`weight` INT(10) NOT NULL , 
+`age` INT(10) NOT NULL ,
+PRIMARY KEY (`personId`)) ENGINE = InnoDB;
