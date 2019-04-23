691e0418c1ab0bdc598a095880a5640b4bc9cdd8
Trey <treyw1850@gmail.com>
2019-04-23 06:53:19 -0400

    styles

---
+++ views/inc/nav.php
@@ -0,0 +1,3 @@
+<nav class='nav'>
+    <h1 class='nav_title'>People Comparer</h1>
+</nav>
--- views/index.php
+++ views/index.php
@@ -22,7 +22,7 @@
     
     <!-- Table -->
     <table>
-        <tr>
+        <tr class='th-row'>
             <th class='th_first-name'>First Name</th>
             <th class='th_last-name'>Last Name</th> 
             <th class='th_height'>Height</th>
@@ -35,11 +35,76 @@
             <td class='td_height'>Smith</td> 
             <td class='td_weight'>6'2</td> 
             <td class='td_age'>160 lbs</td>
-        </tr>        
+        </tr>    
+        
+        <tr>
+            <td class='td_first-name'>Jill</td>
+            <td class='td_last-name'>Smith</td> 
+            <td class='td_height'>Smith</td> 
+            <td class='td_weight'>6'2</td> 
+            <td class='td_age'>160 lbs</td>
+        </tr> 
+
+        <tr>
+            <td class='td_first-name'>Jill</td>
+            <td class='td_last-name'>Smith</td> 
+            <td class='td_height'>Smith</td> 
+            <td class='td_weight'>6'2</td> 
+            <td class='td_age'>160 lbs</td>
+        </tr> 
+
+        <tr>
+            <td class='td_first-name'>Jill</td>
+            <td class='td_last-name'>Smith</td> 
+            <td class='td_height'>Smith</td> 
+            <td class='td_weight'>6'2</td> 
+            <td class='td_age'>160 lbs</td>
+        </tr> 
+
+        <tr>
+            <td class='td_first-name'>Jill</td>
+            <td class='td_last-name'>Smith</td> 
+            <td class='td_height'>Smith</td> 
+            <td class='td_weight'>6'2</td> 
+            <td class='td_age'>160 lbs</td>
+        </tr> 
     </table>
+    
+    <div class="input-row_1">
+        
+    <div>
+        <label for="fn">First Name: </label>
+        <input name = 'fn' type="text">
+    </div>
+
+        <div>
+            <label for="ln">Last Name: </label>
+            <input name = 'ln' type="text">
+        </div>
+
+    </div>
+
+    <div class="input-row_2">
+        
+        <div>
+            <label for="height">Height: </label>
+            <input name = 'height' type="text">
+        </div>
+        <div>
+            <label for="weight">Weight: </label>
+            <input name = 'weight' type="text">
+        </div>
+        <div>
+            <label for="age">Age: </label>
+            <input name = 'age' type="text">
+        </div>
+
+    </div>
+
+
+    <a href="" class='btn_add-new q-btn'>Add New</a>
 </div>
-    
-    
+
     <!-- Footer -->
     <footer>
     
